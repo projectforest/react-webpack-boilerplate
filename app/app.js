2c79f758efc7793d868b8c069ee9a6ed32233b3d
hlinforest <hllin@sfu.ca>
2015-11-03 19:02:00 -0800

    basic rendering

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from "react"; //ES6 syntax to import a package
 require('./css/bootstrap.css')
 
 React.render(
-  React.createElement('h1',{},'Hello World!'),
+  React.createElement('ul', {}, [ //element type, options, content
+    React.createElement('li', {}, 'Team SoloMid'),
+    React.createElement('li', {}, 'Counter Logic Gaming'),
+    React.createElement('li', {}, 'Cloud 9')
+    ]),
   document.body
 );
+
+
+
+
